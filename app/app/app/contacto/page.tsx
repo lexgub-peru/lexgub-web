04b51b121e8c46b60b4d378ace4f6faee4d6c125
lexgub-peru <magallo88@gmail.com>
2025-06-13 12:33:30 -0500

    Create page.tsx

---
+++ app/app/app/contacto/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from '../components/Navbar';
+
+export default function Contacto() {
+  return (
+    <>
+      <Navbar />
+      <main style={{ padding: '2rem' }}>
+        <h2>Contacto</h2>
+        <p>Puedes escribirnos a: <strong>lexgubperu@gmail.com</strong></p>
+      </main>
+    </>
+  );
+}
