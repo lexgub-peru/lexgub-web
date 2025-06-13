bbc02858ec67cde3983ffcc56cfeb0915c8bd794
lexgub-peru <magallo88@gmail.com>
2025-06-12 22:42:20 -0500

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,8 @@
+// app/layout.tsx
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
+}
