a7647262bb615b97b652cd1c563750c689429dee
lexgub-peru <magallo88@gmail.com>
2025-06-12 22:45:02 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
 // app/layout.tsx
+import './globals.css';
+import React from 'react';
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
