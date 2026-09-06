587de386062a1248d07a8cd906c8aebd3bdebd7a
lexgub-peru <magallo88@gmail.com>
2026-09-06 02:57:05 -0500

    feat: cargar estilos editoriales y profesionales

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { IBM_Plex_Sans, Source_Serif_4 } from 'next/font/google';
 import './globals.css';
 import './lexgub-brand.css';
+import './lexgub-editorial.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
